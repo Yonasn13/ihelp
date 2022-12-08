08f58b5df4baaa9fde73d4bdf7b8dae31d7b47b8
Adela Yari <yariadila15@gmail.com>
2022-12-08 11:10:11 +0100

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  <Navbar></Navbar>
+  </>
+  )
 }
+
+
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Link from "next/link"
 import Navbar from "../components/Navbar"
+
 export default function Home(props) {
 
   return (
     <>
-    <Navbar></Navbar>
       <h1>Empty Next App</h1>
       <Link href={"request/helper"} ></Link>
       <Link href={"request/user"} >🔔</Link>
-
     </>
   )
 }
