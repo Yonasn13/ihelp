bfaa7665d574e584c87a59f753770977ef238b4d
Your Name <you@example.com>
2022-12-08 17:59:15 +0100

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from 'react'
 
-<<<<<<< HEAD
-function MyApp({ 
-  Component, 
-  pageProps: { session, ...pageProps} }) {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  )
-=======
 function MyApp({ Component, pageProps }) {
   return
   <Component {...pageProps} />
->>>>>>> user-profile
 }
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-import Link from "next/link";
-=======
 import styles from '../styles/Home.module.css'
 import Image from 'next/image';
 import Navbar from '../components/Navbar';
->>>>>>> 76b0de5e927c9ef827f2197ebc30385b4ce98d53
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
-      <h1>Empty Next App</h1>
-      <Link href={'/user'}>Home</Link >
-=======
       <div className=''>
         <div className={styles.halfPageGreen}>
           <div>
@@ -31,26 +23,25 @@ export default function Home() {
           </div>
           <div className={styles.container2}>
             <div className={styles.helperButton}>
-            <div className='row'>
-              <div className='btn btn-primary'>
-                Helper
-              </div>
-            </div>
-            </div>
-            <div className={styles.helpButtn}>
-            <div className=''>
               <div className='row'>
-                <div className='btn btn-outline-success'>
-                  Help
+                <div className='btn btn-primary'>
+                  Helper
                 </div>
               </div>
             </div>
+            <div className={styles.helpButtn}>
+              <div className=''>
+                <div className='row'>
+                  <div className='btn btn-outline-success'>
+                    Help
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <Navbar></Navbar>
         </div>
       </div>
->>>>>>> 76b0de5e927c9ef827f2197ebc30385b4ce98d53
     </>
   );
 }
