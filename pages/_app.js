9483146de9f9c2d1cd533962cce110d2701898ce
yonase <yonasnuguse1234@gmail.com>
2022-12-08 10:33:09 +0100

    adding directly to main _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/globals.css';
 import { useEffect } from 'react'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps} }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   return (
     <>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
+    </SessionProvider>
     </>
   )
 }
