53a7ccc002043ad0a4e8cbd90c95afdaa13bb54b
Your Name <you@example.com>
2022-12-09 12:24:50 +0100

    changes made

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return
-  <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 
