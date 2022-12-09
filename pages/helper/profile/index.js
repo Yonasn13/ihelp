44bfda378a9fc710dbb666779daba50fc36ad0a1
Adela Yari <yariadila15@gmail.com>
2022-12-09 16:19:18 +0100

    new changes

--- pages/helper/profile/index.js
+++ pages/helper/profile/index.js
@@ -1,7 +1,6 @@
 import helperProfileController from '../../../controllers/helperProfileController';
 import Image from 'next/image';
 import Navbar from '../../../components/Navbar'
-
 import styles from '../../../styles/Home.module.css'
 import Link from 'next/link';
 
@@ -37,30 +36,12 @@ const Profile = props => {
                 <Link href="/helper/requests">
                     <div className='btn btn-primary'>Requests</div>
                 </Link>
-
-const Profile = props => {
-    const helperprofile = props.helperprofile
-    console.log(helperprofile)
-    const helper = helperprofile.User
-    console.log(helper)
-    return (
-        <>
-            <div class="di"
-                style={{ borderRadius: '40px', overflow: 'hidden' }}>
-                <Image
-                    src="/cute-penguin.jpg"
-                    alt="Picture of the author"
-                    width={100}
-                    height={100}
-                />
-            </div>
-            <div>
-                {helper.firstName}
             </div>
             <Navbar></Navbar>
         </>
     )
 }
+
 export async function getServerSideProps(req, res) {
     const userId = 1
     const helperprofile = await helperProfileController.findUser(userId)
