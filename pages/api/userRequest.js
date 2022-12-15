3affe568a079227d0e7400a418f27ffddb29bd9d
yonase <yonasnuguse1234@gmail.com>
2022-12-15 14:37:52 +0100

    modified pages/api/helperResponse.js

--- pages/api/userRequest.js
+++ pages/api/userRequest.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     const request = await requestController.create(req.body)
     res.status(200).redirect(`/user/requests`)
+  } else {
+    return res.status(400).json({ msg: "incorrect method"})
   }
-
-  return res.status(400).json({ msg: "incorrect method"})
 }
