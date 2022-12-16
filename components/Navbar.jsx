b90d83f2dc88758802b0b03be73a424d3b28e696
yonase <yonasnuguse1234@gmail.com>
2022-12-16 17:17:06 +0100

    updating

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,6 +16,7 @@ export default function Navbar(props) {
           <Link href={"/"} className={styles.a}>
             <BsHouseDoor size="30" />
           </Link>
+          
         </div>
         <div className=" p-1 mb-1 rounded">
           <Link href={"/about"} className={styles.a}>
